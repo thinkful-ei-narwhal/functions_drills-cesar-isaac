0e6b1af571e389bfaf4a7b3d7f7ed4a72d2ab0ca
Cesar Morales <cesarlenin2@live.com>
2020-03-16 14:09:32 -0700

    added try catch

--- drills.js
+++ drills.js
@@ -1,6 +1,11 @@
 'use strict';
 
-const getYearOfBirth = age => 2020 - age;
+function getYearOfBirth (age){
+  if(age < 0){
+    throw new Error('Age can not be negative');
+  }
+ return 2020 - age;
+}
 
 function createGreeting(name, age) {
   let yearOfBirth = 2020 - age;
@@ -8,8 +13,10 @@ function createGreeting(name, age) {
   return `I was born in ${yearOfBirth}`;
 }
 
-const greeting1 = createGreeting('Isaac', 22);
-
-console.log(greeting1);
+try{
+const greeting1 = createGreeting('Isaac', -22);
+}catch(error){
+  console.log(error);
+}
 
 console.log(getYearOfBirth(22));
