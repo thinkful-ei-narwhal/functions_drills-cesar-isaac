4fbaf43782b5bbc134a276fea09d09e54230d746
Isaac Elesia <isaacelesia@gmail.com>
2020-03-16 17:40:49 -0500

    Added beyond function

--- drill_2.js
+++ drill_2.js
@@ -1,8 +1,28 @@
 'use strict';
 
-function jediName(firstName, lastName){
-  let fullname =  lastName.slice(0,3)+firstName.slice(0,2);
+function jediName(firstName, lastName) {
+  let fullname = lastName.slice(0, 3) + firstName.slice(0, 2);
   return fullname;
 }
 
 console.log(jediName('Cesa', 'Morales'));
+
+function beyond(num) {
+  if (typeof num === 'number') {
+    console.log('And beyond');
+
+    if (num > 0) {
+      console.log('To infinity');
+    }
+
+    if (num < 0) {
+      console.log('To negative infinity');
+    }
+
+    if (num === 0) {
+      console.log('Staying home');
+    }
+  }
+}
+
+beyond(-2);
