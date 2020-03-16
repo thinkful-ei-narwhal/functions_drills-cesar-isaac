9f5d1e309e19605d716c27d5f31f1cc8bf853825
Isaac Elesia <isaacelesia@gmail.com>
2020-03-16 15:46:02 -0500

    Added yearOfBirth

--- drills.js
+++ drills.js
@@ -1,9 +1,11 @@
 'use strict';
 
 function createGreeting(name, age) {
-  return `Hi, my name is ${name} and I'm ${age} years old`;
+  let yearOfBirth = 2020 - age;
+
+  return `I was born in ${yearOfBirth}`;
 }
 
-const greeting1 = createGreeting('Isaac', '22');
+const greeting1 = createGreeting('Isaac', 22);
 
 console.log(greeting1);
