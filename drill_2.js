1c51726ca285a6ac7cac287d86e40d86e2e282d9
Cesar Morales <cesarlenin2@live.com>
2020-03-16 16:06:54 -0700

    cracking the code function added

--- drill_2.js
+++ drill_2.js
@@ -6,3 +6,34 @@ function jediName(firstName, lastName){
 }
 
 console.log(jediName('Cesa', 'Morales'));
+
+function decode(word){
+  let arr= word.split(' ');
+  let decoded='';
+  arr.forEach(element => {
+    switch (element[0]) {
+    case 'a':
+      decoded += element[1];
+      break;
+    case 'b':
+      decoded += element[2];
+      break; 
+    
+    case 'c':
+      decoded += element[3];
+
+      break;
+    
+    case 'd':
+      decoded += element[4];
+      break; 
+    
+    default:
+      decoded +=' ';
+      break;
+    }
+  });
+  return decoded;
+}
+
+console.log(decode('craft block argon meter bells brown croon droop'));
