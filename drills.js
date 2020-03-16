2564dcb219cbbb25b6a0003c0827068e82aa15d4
Cesar Morales <cesarlenin2@live.com>
2020-03-16 14:39:57 -0700

    fix try/catch

--- drills.js
+++ drills.js
@@ -4,19 +4,20 @@ function getYearOfBirth (age){
   if(age < 0){
     throw new Error('Age can not be negative');
   }
- return 2020 - age;
+  return 2020 - age;
 }
 
-function createGreeting(name, age) {
-  let yearOfBirth = 2020 - age;
-
-  return `I was born in ${yearOfBirth}`;
+function createGreeting(name, fn) {
+  if(typeof name === 'undefined' || typeof fn === 'undefined'){
+    throw new Error('argument not valid');
+  }
+  return `Hi, my name is ${name}, I was born in ${fn}`;
 }
 
 try{
-const greeting1 = createGreeting('Isaac', -22);
+  const greeting1 = createGreeting('cesar',getYearOfBirth(22));
+  console.log(greeting1);
 }catch(error){
-  console.log(error);
+  console.log(error.message);
 }
 
-console.log(getYearOfBirth(22));
