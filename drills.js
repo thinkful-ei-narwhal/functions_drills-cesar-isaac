ba768cb69c70016febc7e1fd35ff2666bb66f43f
Isaac Elesia <isaacelesia@gmail.com>
2020-03-16 15:39:35 -0500

    Added function argurments

--- drills.js
+++ drills.js
@@ -1,6 +1,9 @@
-function createGreeting(){
-return 'Hi, my name is....';
+'use strict';
+
+function createGreeting(name, age) {
+  return `Hi, my name is ${name} and I'm ${age} years old`;
 }
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting('Isaac', '22');
+
 console.log(greeting1);
