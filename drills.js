97d77e28f6c179eb1f4324b1dfedce619c355acd
Isaac Elesia <isaacelesia@gmail.com>
2020-03-16 15:51:15 -0500

    created getYearOfBirth

--- drills.js
+++ drills.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const getYearOfBirth = age => 2020 - age;
+
 function createGreeting(name, age) {
   let yearOfBirth = 2020 - age;
 
@@ -9,3 +11,5 @@ function createGreeting(name, age) {
 const greeting1 = createGreeting('Isaac', 22);
 
 console.log(greeting1);
+
+console.log(getYearOfBirth(22));
